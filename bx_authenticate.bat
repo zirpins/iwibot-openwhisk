4c1adcc0a958e291f4ecf77c88025ff1c362c0f2
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2018-10-18 23:42:13 +0200

    Fix bx_authenticate.bat

--- bx_authenticate.bat
+++ bx_authenticate.bat
@@ -3,6 +3,6 @@
 call local.cmd
 ibmcloud login -a %API_ENDPOINT% -u %BLUEMIX_USERNAME% -p %BLUEMIX_PASSWORD% -o %BLUEMIX_ORGANIZATION% -s %BLUEMIX_SPACE%
 :: target org in cf
-ibmcloud target --cf "%BLUEMIX_ORGANIZATION%"
+ibmcloud target --cf %BLUEMIX_ORGANIZATION%
 :: install wsk plugin for the bluemix cli
 ibmcloud plugin install cloud-functions
