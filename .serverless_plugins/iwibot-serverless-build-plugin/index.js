6223300d6606629e064e031696e507af36f596a7
Armin Kunkel <armin@kunkel24.de>
2018-10-19 20:26:01 +0200

    change local serverless plugin to release

--- .serverless_plugins/iwibot-serverless-build-plugin/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ServerlessBuildPlugin_1 = require("./ServerlessBuildPlugin");
-// tslint:disable-next-line:no-default-export
-exports.default = ServerlessBuildPlugin_1.ServerlessBuildPlugin;
-module.exports = ServerlessBuildPlugin_1.ServerlessBuildPlugin;
