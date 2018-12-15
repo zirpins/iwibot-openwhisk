e2ab12b3e5e669d2e197a1f5b9d3d7b776faf32b
Niklas <nkreutzarek@hotmail.de>
2018-12-15 20:08:19 +0100

    Remove values from config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-workspace_id="4b9aa897-7134-4dbb-9b87-2b4ef6e47fac"
-conversation_service_name="IWIBotConversation"
-weather_insights_service_name="weatherinsights"
+workspace_id="WorkspaceId"
+conversation_service_name="NameOfTheConversationService"
+weather_insights_service_name="NameOfTheWeatherinsightsService"
