50102188edeca33414e841cdaa13a0ff5387e766
Christian Zirpins <zirpins@jona.fritz.box>
2019-02-13 12:27:35 +0100

    update bluemix cli target command

--- bx_authenticate.bat
+++ bx_authenticate.bat
@@ -4,5 +4,6 @@ call local.cmd
 ibmcloud login -a %API_ENDPOINT% -u %BLUEMIX_USERNAME% -p %BLUEMIX_PASSWORD% -o %BLUEMIX_ORGANIZATION% -s %BLUEMIX_SPACE%
 :: target org in cf
 ibmcloud target --cf %BLUEMIX_ORGANIZATION%
+ibmcloud target -o %BLUEMIX_ORGANIZATION% -s %BLUEMIX_SPACE%
 :: install wsk plugin for the bluemix cli
 ibmcloud plugin install cloud-functions
--- bx_authenticate.sh
+++ bx_authenticate.sh
@@ -4,6 +4,6 @@ source local.env
 ibmcloud login -a "$API_ENDPOINT" -u "$BLUEMIX_USERNAME" -p "$BLUEMIX_PASSWORD" \
     -o "$BLUEMIX_ORGANIZATION" -s "$BLUEMIX_SPACE" \
 # target org in cf
-ibmcloud target --cf "$BLUEMIX_ORGANIZATION"
+ibmcloud target -o "$BLUEMIX_ORGANIZATION" -s "$BLUEMIX_SPACE"
 # install wsk plugin for the bluemix cli
 ibmcloud plugin install cloud-functions
