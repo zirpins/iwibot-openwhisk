c4646c5e32b6a177699945c3fc31a2a6b0f5337c
Christian Zirpins <zirpins@jona.fritz.box>
2019-02-22 09:03:35 +0100

    update keys action

--- nodejs-keys/lib/Keys.js
+++ nodejs-keys/lib/Keys.js
@@ -2,44 +2,82 @@ const request = require('request'),
     crypto = require('crypto'),
     openwhisk = require('openwhisk');
 
-require('dotenv').load();
-
 function main(params) {
     if ("__ow_body" in params) { // Für das Testen erforderlich..
         Object.assign(params, JSON.parse(params.__ow_body));
     }
 
     // So bekommst du in den logs mit, welche parameter diese Aktion zur Verfügung hat.
-    console.log("------Template Action started!------");
-    console.log("TemplateAction Params:" + JSON.stringify(params));
+    console.log("------Keys Action started!------");
+    console.log("Keys Action Params:" + JSON.stringify(params));
 
     return new Promise(function (resolve, reject) {
-        // Füge hier deine Aktionen hinzu und rufe bei positivem Feedback die resolve, und bei negativem die reject Funktion auf
+        action( // call session service to negotiate the session id and crypto key
+                name = "/IWIbot_dev/IWIbot_dev/Sessions",
+                blocking = true,
+                result = true,
+                params = params)
 
-        // Get session identifier if any
-        var session_id = params.sid;
+            .then(function (sessions_response) {
+                let session_string, key_string;
 
-        // TODO
-        // call session service to negotiate the session id and crypto key
-        //
-        
-        // mock a 128 bit session identifier
-        if (! session_id) {
-            session_id = crypto.randomBytes(16).toString('hex');
-            console.log("Session id created: " + session_id.toString('hex'));
-        }
+                if ('payload' in sessions_response && "sid" in sessions_response.payload) {
 
-        // 256 bit mock key instead of: encryption_key = crypto.randomBytes(32);
-        encryption_key = new Buffer("b26301cc95648636104ed5c40cd083ec0b507434a809b6186d882f9f3665baa5", "hex");
-        console.log("crypto_key created: " + encryption_key.toString('hex'));
+                    session_string = sessions_response.payload.sid;
 
-        resolve({
-            "payload": {
-                "sid": session_id,
-                "crypto_key": encryption_key.toString('hex'),
-            }
-        });
-    });
+                    if ('session_context' in sessions_response.payload &&
+                        "crypto_key" in sessions_response.payload.session_context) {
+
+                        key_string = sessions_response.payload.session_context.crypto_key;
+
+                    } else {
+
+                        // create and store new 256 bit aes key if required
+                        raw_key = crypto.randomBytes(32);
+                        key_string = raw_key.toString('hex');
+                        console.log("crypto key created: " + key_string);
+
+                        // Put crypto key in session context
+                        params.session_context = {
+                            'crypto_key': key_string
+                        };
+
+                        // Update session
+                        action(
+                            name = "/IWIbot_dev/IWIbot_dev/Sessions",
+                            blocking = false,
+                            result = false,
+                            params = params);
+                    }
+
+                    resolve({
+                        "payload": {
+                            "sid": session_string,
+                            "crypto_key": key_string
+                        }
+                    }); // End if payload.seesion_context.crypto_key
+
+                } else {
+
+                    reject({
+                        "reason": "Sessions unavailable"
+                    });
+
+                } // End if payload.sid
+
+            }); // End action chain
+
+    }); // End new Promise
+}
+
+function action(name, blocking, result, params) {
+    let ow = openwhisk();
+    return ow.actions.invoke({
+        name,
+        blocking,
+        result,
+        params
+    }); // jshint ignore:line
 }
 
 exports.main = main;
