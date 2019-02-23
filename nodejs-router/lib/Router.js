be1201da297e1420448ab565423f8f2741830a01
Christian Zirpins <zirpins@jona.fritz.box>
2019-02-23 20:15:56 +0100

    minor update router

--- nodejs-router/lib/Router.js
+++ nodejs-router/lib/Router.js
@@ -4,7 +4,6 @@ let conversation = require('./conversation');
 // Use this for own Python-Classifer Based Conversation
 //let conversation = require('./classifier-based-conversation/conversation');
 
-
 function main(params) {
 
     console.log("------Router started!------");
@@ -23,14 +22,17 @@ function main(params) {
         return locationEvents.getEventsForPosition(positionObj).then(function (response) {
             console.log("Responding... " + JSON.stringify(response));
             return {
-                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain'},
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'text/plain'
+                },
                 body: JSON.stringify(response),
                 code: 200
-            };       
+            };
         });
     }
 
-    if("__ow_body" in params) { // For testing this action!!
+    if ("__ow_body" in params) { // For testing this action!!
         Object.assign(params, JSON.parse(params.__ow_body));
     }
 
@@ -38,46 +40,48 @@ function main(params) {
         semester = params.semester;
         courseOfStudies = params.courseOfStudies;
     }
-    
+
     if ("position" in params) {
         position = params.position;
     }
 
-    return conversation.sendMessage("conInit" in params, params).then(function (params) {
+    return conversation.sendMessage("conInit" in params, params)
+        .then(function (params) { // conversation succeeded
             params.semester = semester;
             params.courseOfStudies = courseOfStudies;
-            params.position = typeof position !== 'undefined' ? {
-                latitude: position[1],
-                longitude: position[0]
-            } : position;
+            params.position =
+                typeof position !== 'undefined' ? {
+                    latitude: position[1],
+                    longitude: position[0]
+                } : position;
 
             return dispatcher.dispatch(params);
-
-        }, function (reason) {
+        }, function (reason) { // conversation error
             console.error("Conversation Error: " + reason);
-
             throw reason;
-
-        }).then(function (response) {
-
+        })
+        .then(function (response) { // dispatcher succeeded
             return {
-                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain'},
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'text/plain'
+                },
                 body: JSON.stringify(response),
                 code: 200
             };
-
-        }, function (reason) {
+        }, function (reason) { // dispatcher error
             console.log("Dispatcher Error: " + reason);
-
             let response = {};
             response.payload = 'Error occurred';
-
             return {
-                headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain'},
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'text/plain'
+                },
                 body: JSON.stringify(response),
                 code: 200
             };
-
-        });
+        }); // End return
 }
+
 exports.main = main;
