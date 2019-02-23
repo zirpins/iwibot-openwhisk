0b6beaae2c0a0411d3f2aadc9b4d6f55330256fb
Christian Zirpins <zirpins@jona.fritz.box>
2019-02-23 20:12:45 +0100

    add testing for keys and sessions

--- nodejs-keys/test/Keys.iwibot_test.js
+++ nodejs-keys/test/Keys.iwibot_test.js
@@ -1,13 +1,45 @@
-/**
- * Created by Armin on 05.06.2017.
- */
 let request = require('request');
 let expect = require('chai').expect;
-let assert = require('assert');
-let actionUrl = process.env.ACTION_PREFIX_URL + '/test';
+const actionUrl = process.env.ACTION_PREFIX_URL + '/keys';
 
-describe("Test Action Test", function () {
-    it("tests the Test Action", function () {
+describe("Testing 'Keys' action", function () {
+    let session_identifier;
+    let crypto_key;
 
-    });
+    it("returns a novel session identifier and an aes crypto key on first call",
+        (done) => {
+            request.post(actionUrl, function (err, response, body) {
+                result = JSON.parse(body);
+
+                expect(result.payload).to.be.an("object");
+                expect(result.payload.sid).to.be.a("string");
+                session_identifier = result.payload.sid;
+
+                expect(result.payload.crypto_key).to.be.a("string");
+                crypto_key = result.payload.crypto_key;
+
+                done();
+            });
+        });
+
+    it("returns the same aes crypto key for successive calls based on the session identifier",
+        (done) => {
+            request.post({
+                    url: actionUrl,
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        sid: session_identifier
+                    })
+                },
+                function (err, response, body) {
+                    result = JSON.parse(body);
+
+                    expect(result.payload.sid).to.equal(session_identifier, "Session identifier has changed");
+                    expect(result.payload.crypto_key).to.equal(crypto_key, "AES Key has changed");
+
+                    done();
+                });
+        });
 });
--- nodejs-sessions/test/Sessions.iwibot_test.js
+++ nodejs-sessions/test/Sessions.iwibot_test.js
@@ -1,13 +1,42 @@
-/**
- * Created by Armin on 05.06.2017.
- */
 let request = require('request');
 let expect = require('chai').expect;
-let assert = require('assert');
-let actionUrl = process.env.ACTION_PREFIX_URL + '/test';
+const actionUrl = process.env.ACTION_PREFIX_URL + '/sessions';
 
-describe("Test Action Test", function () {
-    it("tests the Test Action", function () {
+describe("Testing 'Sessions' action", function () {
+    let session_identifier;
+    let session_context;
 
-    });
+    it("stores a novel session context and provides a session identifier",
+        (done) => {
+            request.post({
+                    url: actionUrl,
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        session_context: {
+                            foo: "bar"
+                        }
+                    })
+                },
+                function (err, response, body) {
+                    result = JSON.parse(body);
+
+                    expect(result).to.have.property("payload");
+                    expect(result.payload).to.be.an("object");
+                    expect(result.payload).to.have.property("sid");
+                    expect(result.payload.sid).to.be.a("string");
+                    session_identifier = result.payload.sid;
+
+                    expect(result.payload).to.have.property("session_context");
+                    expect(result.payload.session_context).to.be.an("object");
+                    session_context = result.payload.session_context;
+
+                    expect(result.payload.session_context).to.have.property("foo");
+                    expect(result.payload.session_context.foo).to.be.a("string");
+                    expect(result.payload.session_context.foo).to.equal("bar");
+    
+                    done();
+                });
+        });
 });
