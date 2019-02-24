c0912752ff05dcce3d5aa36da5d81b118bcbfcb6
Christian Zirpins <zirpins@jona.fritz.box>
2019-02-24 11:41:15 +0100

    cleaned up wiki action and fimished test

--- nodejs-wikipedia/lib/Wikipedia.js
+++ nodejs-wikipedia/lib/Wikipedia.js
@@ -1,39 +1,13 @@
 var request = require('request');
-var bot = require('nodemw');
-
-// read config from external file
-var client = new bot({
-    "protocol": "https",  // default to 'http'
-    "server": "de.wikipedia.org",  // host name of MediaWiki-powered site
-    "path": "/w",                  // path to api.php script
-    "debug": true,                // is more verbose when set to true
-    "userAgent": "Custom UA",      // define custom bot's user agent
-    "concurrency": 5               // how many API requests can be run in parallel (defaults to 3)
-});
 
 function main(params) {
     return new Promise(function (resolve, reject) {
-        var url = 'http://de.wikipedia.org/w/api.php?action=opensearch&search='+ params.input.text.toLowerCase().replace(" ", "%20") +'&limit=10&namespace=0&format=json';
-        /*client.getArticle(params.input.text, function(err, data) {
-            // error handling
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(JSON.stringify(data, null, 2));
 
-            var responseObject = {};
-            responseObject.payload = "Das habe ich zu " + params.input.text + " gefunden.";
+        var url = 'http://de.wikipedia.org/w/api.php?action=opensearch&search=' +
+            params.input.text.toLowerCase().replace(" ", "%20") + '&limit=10&namespace=0&format=json';
 
-            client.parse(data, params.input.text, function (result) {
-                console.log("paresd Result: " + result);
-                console.log("arguments: " + JSON.stringify(arguments));
-                responseObject.htmlText = JSON.stringify(result);
-                resolve(responseObject);
-            });
-        });*/
-        request(url, function(err, response, data) {
-            if(!err) {
+        request(url, function (err, response, data) {
+            if (!err) {
                 console.log("Params: " + JSON.stringify(params, null, 2));
                 console.log("in callback");
 
@@ -62,25 +36,8 @@ function main(params) {
                 resolve(responseObject);
             }
         }).setMaxListeners(12);
+        
     });
 }
-function convertCertificateTypeToDisplayName(type) {
-    switch(type) {
-        case 'DATA_CONTROL_SHEET':
-            return 'Datenkontrollblatt';
-        case 'CERTIFICATE_OF_MATRICULATION':
-            return 'Immatrikulationsbescheinigung (deutsch)';
-        case 'CERTIFICATE_OF_MATRICULATION_ENGLISH' :
-            return 'Immatrikulationsbescheinigung (englisch)';
-        case 'BAFOEG':
-            return 'BAFÖG-Bescheinigung';
-        case 'KVV':
-            return 'KVV-Bescheinigung';
-        case 'DURATION_OF_STUDY':
-            return 'Studienzeitbescheinigung';
-        case 'IZ_ACCESS':
-            return 'Initiale IZ-Zugangsdaten';
-    }
 
-}
 exports.main = main;
--- nodejs-wikipedia/test/wikipedia.iwibot_test.js
+++ nodejs-wikipedia/test/wikipedia.iwibot_test.js
@@ -4,10 +4,21 @@
 let request = require('request');
 let expect = require('chai').expect;
 let assert = require('assert');
-let actionUrl = process.env.ACTION_PREFIX_URL + '/joke';
+let actionUrl = process.env.ACTION_PREFIX_URL + '/wikipedia';
 
 describe("Wikipedia Action Test", function () {
-    it("returns a description", function () {
-
-    })
+    it("returns a description", function (done) {
+        request.post({
+            headers: {
+                'content-type': 'text/plain'
+            },
+            url: actionUrl,
+            body: JSON.stringify(params)
+        }, function (err, response, body) {
+            body = JSON.parse(body);
+            assert(body.payload);
+            assert(body.htmlText);
+            done();
+        });
+    });
 });
